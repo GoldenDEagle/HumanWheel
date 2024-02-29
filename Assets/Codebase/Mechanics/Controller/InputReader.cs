3037aeaed5774e42431db535848da01859b7d9c1
Ivan <ionufriev95@gmail.com>
2024-02-29 17:36:06 +0600

    Steerable wheel

---
+++ Assets/Codebase/Mechanics/Controller/InputReader.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Codebase.Mechanics.Controller
+{
+    [RequireComponent(typeof(PlayerController))]
+    public class InputReader : MonoBehaviour
+    {
+        private PlayerController _playerController;
+
+        private void Awake()
+        {
+            _playerController = GetComponent<PlayerController>();
+        }
+
+        public void SetDirection(InputAction.CallbackContext context)
+        {
+            var direction = context.ReadValue<Vector2>();
+            _playerController.SetDirection(direction);
+        }
+    }
+}
---
+++ Assets/Codebase/Mechanics/Controller/WheelController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Codebase.Mechanics.Controller
+{
+    public class WheelController : MonoBehaviour
+    {
+        public Transform _wheelModel;
+
+        [HideInInspector] public WheelCollider WheelCollider;
+
+        // Create properties for the CarControl script
+        // (You should enable/disable these via the 
+        // Editor Inspector window)
+        public bool steerable;
+        public bool motorized;
+
+        Vector3 position;
+        Quaternion rotation;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            WheelCollider = GetComponent<WheelCollider>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Get the Wheel collider's world pose values and
+            // use them to set the wheel model's position and rotation
+            WheelCollider.GetWorldPose(out position, out rotation);
+            _wheelModel.transform.position = position;
+            _wheelModel.transform.rotation = rotation;
+        }
+    }
+}
