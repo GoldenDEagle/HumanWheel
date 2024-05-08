8eaaff59e4c80e379887a18a392834dcacd32d08
Ivan <ionufriev95@gmail.com>
2024-05-08 17:10:35 +0400

    Menu scene, hand + more segments

--- Assets/Codebase/Utils/UI/SlidingHand.cs
+++ Assets/Codebase/Utils/UI/SlidingHand.cs
@@ -1,15 +1,16 @@
-﻿using DG.Tweening;
+﻿using Assets.Codebase.RaceElements;
+using DG.Tweening;
 using UnityEngine;
 
-namespace Assets.Codebase.RaceElements
+namespace Assets.Codebase.Utils.UI
 {
-    public class MovingWall : MonoBehaviour
+    public class SlidingHand : MonoBehaviour
     {
         [SerializeField] private MovementDirection _startingDirection;
         [SerializeField] private float _movementTime = 2f;
-        [SerializeField] private Transform _wall;
-        [SerializeField] private Transform _rightBorder;
-        [SerializeField] private Transform _leftBorder;
+        [SerializeField] private RectTransform _hand;
+        [SerializeField] private RectTransform _rightBorder;
+        [SerializeField] private RectTransform _leftBorder;
 
         private void OnEnable()
         {
@@ -18,7 +19,7 @@ namespace Assets.Codebase.RaceElements
 
         private void OnDisable()
         {
-            DOTween.Kill(_wall);
+            DOTween.Kill(_hand);
         }
 
         private void StartMovement()
@@ -38,18 +39,12 @@ namespace Assets.Codebase.RaceElements
 
         private void MoveRight()
         {
-            _wall.DOMoveX(_rightBorder.position.x, _movementTime).SetEase(Ease.InOutSine).OnComplete(MoveLeft);
+            _hand.DOAnchorPosX(_rightBorder.anchoredPosition.x, _movementTime).SetEase(Ease.InOutSine).OnComplete(MoveLeft);
         }
 
         private void MoveLeft()
         {
-            _wall.DOMoveX(_leftBorder.position.x, _movementTime).SetEase(Ease.InOutSine).OnComplete(MoveRight);
+            _hand.DOAnchorPosX(_leftBorder.anchoredPosition.x, _movementTime).SetEase(Ease.InOutSine).OnComplete(MoveRight);
         }
-    }
-
-    public enum MovementDirection
-    {
-        Right,
-        Left,
     }
 }
