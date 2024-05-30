fcb3d636230a60ccf00bbe05ac543f73eb1fb7f8
Ivan <ionufriev95@gmail.com>
2024-05-30 15:46:14 +0400

    Portrait interface

---
+++ Assets/Codebase/Optimizations/Cameras/TitleScreenCameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Assets.Codebase.Optimizations.Cameras
+{
+    public class TitleScreenCameraController : MonoBehaviour
+    {
+        [SerializeField] private float _verticalOrthSize = 12f;
+        [SerializeField] private float _horizontalOrthSize = 6f;
+
+        private Camera _mainCamera;
+
+        private void Awake()
+        {
+            _mainCamera = GetComponent<Camera>();
+        }
+
+        private void Update()
+        {
+            if (Screen.width >= Screen.height)
+            {
+                _mainCamera.orthographicSize = _horizontalOrthSize;
+            }
+            else
+            {
+                _mainCamera.orthographicSize = _verticalOrthSize;
+            }
+        }
+    }
+}
